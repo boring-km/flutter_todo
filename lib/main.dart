080bc9a4f6b3d3133dbd76162d6157341ecf6d27
navyCK <ckck81026@gmail.com>
2021-01-19 15:35:01 +0900

    [TD-07-ck] main.dart 커밋 class 이름 변경, List 생성, 할 일 입력하면 리스트에 추가
    아직 초기 단계 => 변경 해야 할 것
    1. 먼저 입력된 할 일이 아래로 내려가는 현상
    2. 아이디 제거
    3. 아이콘을 RadioButton으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
 
+const String _name = "navyCK";
+
 void main() {
-  runApp(MyApp());
+  runApp(MyToDoApp());
 }
 
-
-class MyApp extends StatelessWidget {
+class MyToDoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter To Do',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'To Do'),
+      home: MyToDo(title: 'To Do'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyToDo extends StatefulWidget {
+  MyToDo({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyToDoState createState() => _MyToDoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyToDoState extends State<MyToDo> with TickerProviderStateMixin {
   final _todoTextEditController = TextEditingController();
+  final List<TodoList> _list = <TodoList>[];
   DateTime selectedDate = DateTime.now();
+
+  bool _isComposing = false;
 
   _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
@@ -49,72 +53,137 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        "${selectedDate.toLocal()}".split(' ')[0],
-                        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    IconButton(
-                        icon: Icon(Icons.date_range),
-                        color: Colors.blue,
-                        onPressed: () => _selectDate(context),
-                    ),
-                  ],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // 주 축 기준 중앙
+              crossAxisAlignment: CrossAxisAlignment.center, // 교차 축 기준 중앙
+              children: <Widget> [
+                Text(
+                  "${selectedDate.toLocal()}".split(' ')[0],
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-              ),
+                IconButton(
+                  icon: Icon(Icons.date_range),
+                  color: Colors.blue,
+                  onPressed: () => _selectDate(context),
+                ),
+              ],
+            ),
 
-              SizedBox(
-                height: 100,
-              ),
-
-              Padding(
+            Flexible(
+              child: ListView.builder(
                 padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget> [
-                    Flexible(
-                      child: TextFormField(
-                        controller: _todoTextEditController,
-                        validator: (value) {
-                          if (value.trim().isEmpty) {
-                            return '할 일을 입력하세요.';
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                          hintText: '할 일을 입력하세요.',
-                        ),
-                        onChanged: (text) {
-                          print(text);
-                        },
-                      ),
-                    ),
-                    Container(
-                      child: IconButton(
-                        color: Colors.blue,
-                        icon: Icon(Icons.send),
-                        onPressed: () {
+                reverse: false,
+                itemCount: _list.length,
+                itemBuilder: (_, index) => _list[index],
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-                        },
-                      ),
-                    )
-                  ],
+  Widget _buildTextComposer() {
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: <Widget> [
+              Flexible(
+                child: TextField(
+                  controller: _todoTextEditController,
+                  decoration: InputDecoration(
+                    hintText: '할 일을 입력하세요.',
+                  ),
+                  onChanged: (text) {
+                    print(text);
+                    setState(() {
+                      _isComposing = text.length > 0;
+                    });
+                  },
+                  onSubmitted: _isComposing ? _handleSubmitted : null,
                 ),
               ),
+              Container(
+                child: IconButton(
+                  color: Colors.blue,
+                  icon: Icon(Icons.send),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_todoTextEditController.text)
+                      : null,
+                ),
+              )
             ],
           ),
+        )
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _todoTextEditController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    TodoList list = TodoList(
+      text: text,
+      animationController: AnimationController(
+        duration: Duration(milliseconds: 700),
+        vsync: this,
+      ),
+    );
+    setState(() {
+      _list.insert(0, list);
+    });
+    list.animationController.forward();
+  }
+}
+
+
+class TodoList extends StatelessWidget {
+  final String text;
+  final AnimationController animationController;
+
+  TodoList({this.text, this.animationController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor:
+      CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(_name, style: Theme.of(context).textTheme.headline6),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
