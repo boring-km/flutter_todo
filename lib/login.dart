affc898c38170211a67f876c4c88e3b72b888f19
navyCK <ckck81026@gmail.com>
2021-02-03 14:32:35 +0900

    [TD-10-ck] 회원가입 틀 만들기 수정

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_todo/signup.dart';
+import 'package:flutter_todo/main.dart';
+
+void main() {
+  runApp(MyToDoApp());
+}
+
+class MyToDoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter To Do',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+      initialRoute: '/login',
+      onGenerateRoute: _getRoute,
+    );
+  }
+
+  Route<dynamic> _getRoute(RouteSettings settings) {
+    if (settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void> (
+      settings : settings,
+      builder: (BuildContext context) => LoginPage(),
+      fullscreenDialog: true,
+    );
+  }
+}
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,23 +42,25 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _passwordConfirmController = TextEditingController();
+  bool _visible = true;
 
   Future<bool> _onBackPressed() {
     return showDialog(
         context: context,
         builder: (context) => AlertDialog(
-              title: Text("Do you want to exit the app?"),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text("NO"),
-                  onPressed: () => Navigator.pop(context, false),
-                ),
-                FlatButton(
-                  child: Text("yes"),
-                  onPressed: () => SystemNavigator.pop(),
-                )
-              ],
-            ));
+          title: Text("Do you want to exit the app?"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("NO"),
+              onPressed: () => Navigator.pop(context, false),
+            ),
+            FlatButton(
+              child: Text("yes"),
+              onPressed: () => SystemNavigator.pop(),
+            )
+          ],
+        ));
   }
 
   @override
@@ -55,7 +88,7 @@ class _LoginPageState extends State<LoginPage> {
                 controller: _usernameController,
                 decoration: InputDecoration(
                   filled: true,
-                  labelText: 'Username',
+                  labelText: 'ID',
                 ),
               ),
               SizedBox(height: 12.0),
@@ -67,28 +100,89 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 obscureText: true,
               ),
-              ButtonBar(
-                children: <Widget>[
-                  FlatButton(
-                    child: Text('Sign Up'),
-                    onPressed: () {
-                      // _usernameController.clear();
-                      // _passwordController.clear();
-                      Navigator.of(context, rootNavigator: true).push(
-                        CupertinoPageRoute<void>(
-                          fullscreenDialog: false,
-                          builder: (BuildContext context) => Sign_up(),
-                        ),
-                      );
-                    },
+              SizedBox(height: 12.0),
+              AnimatedOpacity(
+                opacity: _visible ? 0.0 : 1.0,
+                duration: Duration(milliseconds: 500),
+                child: Visibility(
+                  visible: !_visible,
+                  child: TextField(
+                    controller: _passwordConfirmController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: 'Password Confirm',
+                    ),
+                    obscureText: true,
                   ),
-                  RaisedButton(
-                    child: Text('LOGIN'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
+
+
+                ),
+              ),
+              AnimatedOpacity(
+                opacity: _visible ? 1.0 : 0.0,
+                duration: Duration(milliseconds: 500),
+                child: Visibility(
+                  visible: _visible,
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text('Sign Up'),
+                        onPressed: () {
+                          _usernameController.clear();
+                          _passwordController.clear();
+                          _passwordConfirmController.clear();
+                          setState(() {
+                            _visible = !_visible;
+                          });
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('LOGIN'),
+                        onPressed: () {
+                          String name;
+                          if(_usernameController.text.isNotEmpty)
+                            name = _usernameController.text;
+                          else
+                            name = "test";
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => TodoMain(name)));
+                        },
+                      ),
+                    ],
                   ),
-                ],
+                ),
+              ),
+              AnimatedOpacity(
+                opacity: _visible ? 0.0 : 1.0,
+                duration: Duration(milliseconds: 500),
+                child: Visibility(
+                  visible: !_visible,
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text('Cancel'),
+                        onPressed: () {
+                          _usernameController.clear();
+                          _passwordController.clear();
+                          _passwordConfirmController.clear();
+                          setState(() {
+                            _visible = !_visible;
+                          });
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Sign Up'),
+                        onPressed: () {
+                          _usernameController.clear();
+                          _passwordController.clear();
+                          _passwordConfirmController.clear();
+                          setState(() {
+                            _visible = !_visible;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
